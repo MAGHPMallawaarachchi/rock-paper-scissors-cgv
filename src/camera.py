78a64a3a79cc9ecc4904504276033f0c7e98b0ec
MKPNJoanne <97240800+MKPNJoanne@users.noreply.github.com>
2025-05-05 18:52:54 +0530

    feat(camera): initialize webcam

--- src/camera.py
+++ src/camera.py
@@ -1,2 +1,11 @@
 import cv2
+import os
+
+def capture_image(save_path="../images/input.jpg"):
+    os.makedirs(os.path.dirname(save_path), exist_ok=True) 
+
+#initialize the webcam
+cam = cv2.VideoCapture(0) 
+if not cam.isOpened():
+    raise IOError("Cannot open webcam")
 
