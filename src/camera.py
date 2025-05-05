ec50e5c0f9f34c04ca76bc05df4ce61e6a5d4fd9
MKPNJoanne <97240800+MKPNJoanne@users.noreply.github.com>
2025-05-05 19:08:46 +0530

    feat(camera): capture /save frame from webcam

--- src/camera.py
+++ src/camera.py
@@ -5,7 +5,16 @@ def capture_image(save_path="../images/input.jpg"):
     os.makedirs(os.path.dirname(save_path), exist_ok=True) 
 
 #initialize the webcam
-cam = cv2.VideoCapture(0) 
-if not cam.isOpened():
-    raise IOError("Cannot open webcam")
+    cam = cv2.VideoCapture(0) 
+    if not cam.isOpened():
+        raise IOError("Cannot open webcam")
 
+#read frame and save 
+    ret, frame = cam.read()
+    if ret:
+        cv2.imwrite(save_path, frame)
+        cam.release()
+        return save_path
+    else:
+        cam.release()
+        raise ValueError("Failed to capture image")
